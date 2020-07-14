019a9d5a2e28cd8923a8332ec71c7931f245c6e5
Surat <Surat.n@rmutsvmail.com>
2020-07-14 17:54:41 +0700

    add code 4 lines

--- src/Lab1/HelloWorld.java
+++ src/Lab1/HelloWorld.java
@@ -5,6 +5,10 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World !!!");
         System.out.println("Surat Nakthap");
+        System.out.println("oil");
+        System.out.println("21");
+        System.out.println("155");
+        System.out.println("0626377581");
 
     }
 }
