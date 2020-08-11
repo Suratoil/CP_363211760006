4e632fbab40a71edfe38035e64ba7476a8cb5744
surat <surat.n@rmutsvmail.com>
2020-08-11 18:42:45 +0700

    Lab 2 : basic input

---
+++ src/Lab3/Example_if.java
@@ -0,0 +1,24 @@
+package Lab3;
+
+import java.util.Scanner;
+
+public class Example_if{
+    public static void main(String[] args){
+
+        int x,y;
+        x = 10;
+        y = 20;
+        if (x<y){
+            //......statement of if
+            System.out.print("x grater than y.");
+        }
+        System.out.println("Good Bye");
+
+
+
+
+
+
+
+    }//main
+}//class
