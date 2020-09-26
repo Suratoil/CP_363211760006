8d6d25c303f0a5d989438251e557eb71050502dc
surat <surat.n@rmutsvmail.com>
2020-09-26 12:14:05 +0700

    Lab 2 : basic input

---
+++ src/Lad5/BasicArray2D.java
@@ -0,0 +1,44 @@
+package Lad5;
+
+import java.util.Scanner;
+
+public class BasicArray2D {
+    public static void main (String[] args){
+        int number[][] = new int [2][3]; // 2*3 = 6 blocks
+
+        //input Data to array 2D
+        number = inputData (number);
+        //display data to  array 2 D
+        displayArray(number);
+
+
+    }
+
+    private static void displayArray(int[][] number) {
+        System.out.println("Data in array 2D:  " );
+        for (int i = 0; i <number.length ; i++) { //row
+            for (int j = 0; j <number[i].length ; j++) { //column
+                System.out.print(number[i][j]+"  ");  //number[0][0]:
+
+
+
+            }
+System.out.println();
+        }
+    }
+
+    private static int[][] inputData(int[][] number) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Pleasa,input integers to array 2D:  ");
+        for (int i = 0; i < number.length; i++) { //row
+            for (int j = 0; j < number[i].length; j++) { //column
+                System.out.print("number[" + i + "][" + j + "]:");//number[0][0]:
+                number[i][j] = sc.nextInt();
+
+
+            }
+
+        }
+        return  number;
+    }
+}
